de92e5afcb1821bbd7fd1fd9a6d404ff8a1c57ca
Sergio CM <sergio-home@Sergios-MacBook-Pro.local>
2022-01-21 20:19:31 +0000

    End of Lesson 6

--- main.js
+++ main.js
@@ -11,5 +11,23 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        
+        // Add to the cart counter
+        addToCart(){
+            this.cart += 1
+        },
+
+        // Remove From the cart counter
+        removeFromCart(){
+            this.cart -= 1
+        },
+
+        // Change image when hovering color
+        updateImage(variantImage){
+            // The image property will be updated
+            this.image = variantImage
+        }
     }
 })
