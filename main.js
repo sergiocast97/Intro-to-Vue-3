1b92b09aefa5cb647eec1872a5e380d133750ed5
Sergio CM <sergio-home@Sergios-MacBook-Pro.local>
2022-01-25 23:50:48 +0000

    End of Lesson 10

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
-    methods: {}
+    methods: {
+        // Use the emitted event to update the cart
+        updateCart(id){
+            this.cart.push(id)
+        },
+        // Remove from Cart
+        removeFromCart(id){
+            const index = this.cart.indexOf(id)
+            if (index > -1){
+                this.cart.splice(index, 1)
+            }
+        }
+    }
 })
